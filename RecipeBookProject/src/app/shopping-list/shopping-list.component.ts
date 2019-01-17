7f6b93995223094b0716a74d23bb02e953401dfc
mav80 <mav80prokonto.pl>
2019-01-17 23:38:36 +0100

    Add inredient model, display some example ingredients.

--- RecipeBookProject/src/app/shopping-list/shopping-list.component.ts
+++ RecipeBookProject/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient = [
+    new Ingredient('schab', 1),
+    new Ingredient('ziemniaki', 5),
+    new Ingredient('sałatka', 2)
+  ];
 
   constructor() { }
 
