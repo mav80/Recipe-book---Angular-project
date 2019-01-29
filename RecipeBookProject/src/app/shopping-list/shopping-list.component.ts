efc42dfcf7fc7f43b24ad0ee3f642481014f261f
mav80 <mav80@prokonto.pl>
2019-01-29 22:07:09 +0100

    Project cleanup. Using subject instead of emitter in shopping list service. Add unsubscribe in shopping list component to prevent memory leak.

--- RecipeBookProject/src/app/shopping-list/shopping-list.component.ts
+++ RecipeBookProject/src/app/shopping-list/shopping-list.component.ts
@@ -1,24 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[] = [];
+  shoppingListServiceSubscribtion: Subscription;
 
-  constructor(private shoppinglistService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
-    this.ingredients = this.shoppinglistService.getIngredients();
-    this.shoppinglistService.ingredientsChanged.subscribe((
+    this.ingredients = this.shoppingListService.getIngredients();
+    this.shoppingListServiceSubscribtion = this.shoppingListService.ingredientsChanged.subscribe((
       ingredients: Ingredient[]) => {
       this.ingredients = ingredients;
       }
     );
+  }
+
+
+  ngOnDestroy(): void {
+    this.shoppingListServiceSubscribtion.unsubscribe();
   }
 
 }
--- RecipeBookProject/src/app/shopping-list/shopping-list.service.ts
+++ RecipeBookProject/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,10 @@
-import {EventEmitter, Injectable} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
+import {Subject} from 'rxjs';
+import {Injectable} from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('beef', 17),
@@ -17,7 +18,7 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
@@ -26,7 +27,7 @@ export class ShoppingListService {
     // emitting a lot of events we use new operator which spreads
     // the list into single elements and then add them and emit single event
 
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
 }
