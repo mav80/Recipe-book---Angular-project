3e239a9559deedcd2e682f4f77837cb1b1b75c13
mav80 <mav80@prokonto.pl>
2019-02-03 19:56:54 +0100

    Recipes list - new/edited recipes are now correctly saved from Recipes tab.

--- RecipeBookProject/src/app/recipes/recipe.service.ts
+++ RecipeBookProject/src/app/recipes/recipe.service.ts
@@ -2,9 +2,11 @@ import {Recipe} from './recipe.model';
 import {Injectable} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
+import {Subject} from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe('Pork chop with potatoes and salad',
@@ -37,6 +39,16 @@ export class RecipeService {
 
   getRecipe(index: number) {
     return this.recipes[index];
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
 }
