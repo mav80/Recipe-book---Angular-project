9a589a8d1b84ce1716ca2ca643078f69dcbce9d1
mav80 <mav80@prokonto.pl>
2019-02-05 23:20:27 +0100

    Make sure recipe has at least empty ingredients table on fetch from database.

--- RecipeBookProject/src/app/shared/data-storage.service.ts
+++ RecipeBookProject/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +16,18 @@ export class DataStorageService {
   }
 
   getRecipes() {
-    this.http.get('https://my-recipe-book-mav.firebaseio.com/recipes.json').subscribe(
-      (response: Response) => {
+    this.http.get('https://my-recipe-book-mav.firebaseio.com/recipes.json').pipe
+    (map((response: Response) => {
         const recipes: Recipe[] = response.json();
+        for (const recipe of recipes) {
+          if (!recipe['ingredients']) {
+            recipe['ingredients'] = [];
+          }
+        }
+        return recipes;
+      }
+    )).subscribe(
+      (recipes: Recipe[]) => {
         console.log(recipes);
         this.recipeService.setRecipes(recipes);
 
