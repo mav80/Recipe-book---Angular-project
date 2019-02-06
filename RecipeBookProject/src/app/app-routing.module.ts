d028577573f1b71cd83a56b044e04cc144765c5f
mav80 <mav80@prokonto.pl>
2019-02-06 22:19:16 +0100

    User is redirected to the main page if accessing unknown route.

--- RecipeBookProject/src/app/app-routing.module.ts
+++ RecipeBookProject/src/app/app-routing.module.ts
@@ -19,7 +19,8 @@ const appRoutes: Routes = [
     ]},
   {path: 'shopping-list', component: ShoppingListComponent},
   {path: 'signup', component: SignupComponent},
-  {path: 'signin', component: SigninComponent}
+  {path: 'signin', component: SigninComponent},
+  {path: '**', redirectTo: '/recipes' }
 
 ];
 
