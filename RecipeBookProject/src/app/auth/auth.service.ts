350eefb2f8cf3631b722ff2b4907d5637a4f1c3a
mav80 <mav80@prokonto.pl>
2019-02-06 19:27:11 +0100

    Signup button correctly adds user to the database.

--- RecipeBookProject/src/app/auth/auth.service.ts
+++ RecipeBookProject/src/app/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,8 @@ export class AuthService {
   constructor() { }
 
   signupUser(email: string, password: string) {
-
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch( error => console.log('SignUp error: ' + error));
   }
 
 }
