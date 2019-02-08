2f27f78a32c4e8dfa4b0a75686732a9ba52ceb50
mav80 <mav80@prokonto.pl>
2019-02-08 22:14:24 +0100

    Add lazy loading for recipes module.

--- RecipeBookProject/src/app/app.module.ts
+++ RecipeBookProject/src/app/app.module.ts
@@ -7,21 +7,21 @@ import {AppRoutingModule} from './app-routing.module';
 import {RecipeService} from './recipes/recipe.service';
 import { HttpModule } from '@angular/http';
 import {DataStorageService} from './shared/data-storage.service';
-import {RecipesModule} from './recipes/recipes.module';
 import {SharedModule} from './shared/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {AuthModule} from './auth/auth.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
   ],
   imports: [
     AuthModule,
     BrowserModule,
     HttpModule,
-    RecipesModule,
     SharedModule,
     ShoppingListModule,
     AppRoutingModule    // make sure this one is at the end of imports!
--- RecipeBookProject/src/app/recipes/recipes-routing.module.ts
+++ RecipeBookProject/src/app/recipes/recipes-routing.module.ts
@@ -7,7 +7,7 @@ import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
 import {NgModule} from '@angular/core';
 
 const recipesRoutes: Routes = [
-  {path: 'recipes', component: RecipesComponent, children: [
+  {path: '', component: RecipesComponent, children: [
       {path: '', component: RecipeStartComponent},
       {path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
       {path: ':id', component: RecipeDetailComponent},
