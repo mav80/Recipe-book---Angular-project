ff4fab515b8268ee05c2b37317a796e24f0254d1
mav80 <mav80@prokonto.pl>
2019-02-09 21:19:22 +0100

    Create core module, make app.module leaner.

--- RecipeBookProject/src/app/app.module.ts
+++ RecipeBookProject/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {HeaderComponent} from './header/header.component';
 import {AppRoutingModule} from './app-routing.module';
 import {RecipeService} from './recipes/recipe.service';
 import { HttpModule } from '@angular/http';
@@ -10,13 +9,11 @@ import {DataStorageService} from './shared/data-storage.service';
 import {SharedModule} from './shared/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {AuthModule} from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
+import {CoreModule} from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
   ],
   imports: [
     AuthModule,
@@ -24,6 +21,7 @@ import { HomeComponent } from './home/home.component';
     HttpModule,
     SharedModule,
     ShoppingListModule,
+    CoreModule,
     AppRoutingModule    // make sure this one is at the end of imports!
   ],
   providers: [RecipeService, DataStorageService],
