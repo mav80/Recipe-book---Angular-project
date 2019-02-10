b47fe6d74e921e9b99961fa8bd6b305a5d05ce9a
mav80 <mav80@prokonto.pl>
2019-02-10 18:30:31 +0100

    Use new HttpClient isntead of old Http in data storage service to fetch/save data.

--- RecipeBookProject/src/app/shared/data-storage.service.ts
+++ RecipeBookProject/src/app/shared/data-storage.service.ts
@@ -1,32 +1,31 @@
 import { Injectable } from '@angular/core';
-import {Http, Response} from '@angular/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
 import {map} from 'rxjs/operators';
 import {AuthService} from '../auth/auth.service';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
 
-  constructor(private http: Http,
+  constructor(private httpClient: HttpClient,
               private recipeService: RecipeService,
               private authService: AuthService) { }
 
   storeRecipes() {
     const token = this.authService.getToken();
-    return this.http.put('https://my-recipe-book-mav.firebaseio.com/recipes.json?auth='  + token, this.recipeService.getRecipes());
+    return this.httpClient.put('https://my-recipe-book-mav.firebaseio.com/recipes.json?auth='  + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
 
     const token = this.authService.getToken();
 
-    this.http.get('https://my-recipe-book-mav.firebaseio.com/recipes.json?auth=' + token).pipe
-    (map((response: Response) => {
-        const recipes: Recipe[] = response.json();
-        for (const recipe of recipes) {
+    this.httpClient.get<Recipe[]>('https://my-recipe-book-mav.firebaseio.com/recipes.json?auth=' + token).pipe
+    (map((recipes) => {
+        for (let recipe of recipes) {
           if (!recipe['ingredients']) {
             recipe['ingredients'] = [];
           }
